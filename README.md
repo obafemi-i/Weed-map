863df5c6dbd0cbe7d429acc0f206e0e4059be0a2
Obafemi <obaff76@gmail.com>
2024-02-12 14:11:09 +0100

    final product

---
+++ README.md
@@ -0,0 +1,7 @@
+### Weed-Maps Scraper
+
+Install dependencies before running the script, to install dependecies run the command below in the terminal `pip install -r requirements.txt`
+
+A sample output of the scraper is in the sample.csv file.
+
+The scraped results are saved in a "output.csv" file. If you wish to change the name of the file, go to line number 6, the output variable in the main.py file and change the value. Like this; output = 'output.csv' , change to, output = 'anything-you-want.csv'
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import requests
 from selectolax.parser import HTMLParser
+import os
+import pandas as pd
 
 
-url = 'https://weedmaps.com/dispensaries/mr-nice-guy-costa-mesa?page=1'
+output = 'play.csv'
 
 session = requests.Session()
+
 
 def get_html(url):
     header = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'}
@@ -23,20 +26,49 @@ def parse_attribute_error(html, selector):
         return None
 
 
-content = get_html(url)
+def export_to_csv(ads: list):
+    file_exists = os.path.isfile(output)
 
-products = content.css('li[data-test-id=menu-item-list-item]')
-for product in products:
-    category = parse_attribute_error(product, 'div[data-testid=menu-item-category]')
-    name = parse_attribute_error(product, 'div[data-testid=menu-item-title]')
-    price = parse_attribute_error(product, 'div.Text-sc-42443ba-0.PriceText-sc-b03e0af1-1.bpRqjV.fpIvWp')
-    deals = parse_attribute_error(product, 'div.src__Box-sc-1sbtrzs-0.klazS').split(' ')[0]
+    output_df = pd.DataFrame(ads)
 
-    info_dict = {
-        'category': category,
-        'Product': name,
-        'Price': price,
-        'Deals': deals
-    }
+    if not file_exists:
+        output_df.to_csv(output, index=False)  
+    else:
+        output_df.to_csv(output, mode='a', header=False, index=False)
 
-    print(info_dict)
+
+def extract_and_save_info(url):
+    content = get_html(url)
+
+    products = content.css('li[data-test-id=menu-item-list-item]')
+    for product in products:
+        category = parse_attribute_error(product, 'div[data-testid=menu-item-category]')
+        name = parse_attribute_error(product, 'div[data-testid=menu-item-title]')
+        price = parse_attribute_error(product, 'div.Text-sc-42443ba-0.PriceText-sc-b03e0af1-1.bpRqjV.fpIvWp')
+        deals = parse_attribute_error(product, 'div.src__Box-sc-1sbtrzs-0.klazS').split(' ')[0]
+
+        info_list = []
+
+        info_dict = {
+            'category': category,
+            'Product': name,
+            'Price': price,
+            'Deals': deals
+        }
+
+        info_list.append(info_dict)
+
+        export_to_csv(info_list)
+
+
+def main():
+    for x in range(1, 39):       
+        url = f'https://weedmaps.com/dispensaries/mr-nice-guy-costa-mesa?page={x}'
+        extract_and_save_info(url)
+        print(f'Page {x} extracted. Moving on...')
+    
+    print('All done')
+
+
+if __name__=='__main__':
+    main()
